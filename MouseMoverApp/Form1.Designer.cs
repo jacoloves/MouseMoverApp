d8d5e686e5cd9f0daeef4694bc110a6aa175e464
jacoloves <5511068t@gmail.com>
2024-04-09 12:29:52 +0900

    プロジェクト ファイルを追加します。

---
+++ MouseMoverApp/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace MouseMoverApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start_button = new System.Windows.Forms.Button();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(204, 192);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(100, 30);
+            this.start_button.TabIndex = 0;
+            this.start_button.Text = "START";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // stop_button
+            // 
+            this.stop_button.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.stop_button.Location = new System.Drawing.Point(405, 192);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(100, 30);
+            this.stop_button.TabIndex = 1;
+            this.stop_button.Text = "STOP";
+            this.stop_button.UseVisualStyleBackColor = true;
+            this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stop_button);
+            this.Controls.Add(this.start_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Button stop_button;
+    }
+}
+
---
+++ MouseMoverApp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+//using System.Threading;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace MouseMoverApp
+{
+
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int MOUSEEVENT_MOVE = 0x0001;
+        private Timer mouseMoveTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            mouseMoveTimer = new Timer();
+            mouseMoveTimer.Interval = 5000;
+            mouseMoveTimer.Tick += MouseMoveTiemr_Tick;
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            mouseMoveTimer.Start();
+        }
+
+        private void stop_button_Click(object sender, EventArgs e)
+        {
+            mouseMoveTimer.Stop();
+        }
+
+        private void MouseMoveTiemr_Tick(object sender, EventArgs e)
+        {
+            mouse_event(MOUSEEVENT_MOVE, 100, 0, 0, 0);
+            mouse_event(MOUSEEVENT_MOVE, -100, 0, 0, 0);
+        }
+    }
+}
